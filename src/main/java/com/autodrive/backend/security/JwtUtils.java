56f14a7c0ad310f0e7ae452c0c50102ed9242246
IgorPszczola <251208@edu.p.lodz.pl>
2026-05-29 00:03:39 +0200

    feat: add JwtUtils

---
+++ src/main/java/com/autodrive/backend/security/JwtUtils.java
@@ -0,0 +1,55 @@
+package com.autodrive.backend.security;
+
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+
+
+@Component
+public class JwtUtils {
+    
+    @Value ("${autodrive.app.jwtSecret}")
+    private String jwtSecret;
+
+
+    private final int jwtExpirationMs = 86400000;
+
+    private SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateJwtToken(String email){
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String getEmailFromJwtToken(String token){
+        return Jwts.parser()
+            .verifyWith(getSigningKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+            .getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken){
+        try {
+            Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(authToken);
+            return true; 
+        } catch (JwtException | IllegalArgumentException e) {
+            System.err.println("Invalid JWT token: " + e.getMessage());
+        }
+        return false;
+    }
+}
