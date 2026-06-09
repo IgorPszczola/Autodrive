a15bc53f0768a9d84ff699240543685f6a79bd79
Klaudiusz Tamioła <tamiola.klaudiusz@gmail.com>
2026-06-09 05:50:47 +0200

    fix

--- frontend/nuxt/nuxt.config.ts
+++ frontend/nuxt/nuxt.config.ts
@@ -1,3 +1,5 @@
+const backendUrl = process.env.NUXT_PUBLIC_BACKEND_URL || 'http://localhost:8080'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -6,7 +8,7 @@ export default defineNuxtConfig({
   modules: ['@unocss/nuxt', 'vuetify-nuxt-module'],
   routeRules: {
     '/api/**': {
-      proxy: 'http://localhost:8080/api/**',
+      proxy: `${backendUrl}/api/**`,
     },
   },
 })
